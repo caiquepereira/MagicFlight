6a13002eecf1132be3d1669b780d67f33d1b38f2
Caique de Paula Pereira <caiquepereira@gmail.com>
2015-04-25 18:00:36 -0300

    Song bug fixed

--- MagicFlight/GameMenuScene.m
+++ MagicFlight/GameMenuScene.m
@@ -20,7 +20,6 @@
     AVAudioPlayer *musicPlayer;
     SKSpriteNode *gameCenterButton;
     NSString *leaderboardIdentifier;
-    GameScene *gameScene;
     
     GameMenuSceneViewController *menuViewController;
 }
@@ -50,8 +49,6 @@
         
         gameCenterButton = [self makeGameCenterButton];
         [self addChild:gameCenterButton];
-        
-        gameScene = [[GameScene alloc]init];
         
         leaderboardIdentifier=@"Best_Score_Of_The_App";
         
@@ -135,6 +132,10 @@
         }];
         
         [playButton runAction:startGame];
+    }
+    
+    if ([node.name isEqualToString:@"audioInactive"]) {
+        NSLog(@"EU");
     }
     
     if ([node.name isEqualToString:@"gameCenterButton"] && [GKLocalPlayer localPlayer].authenticated) {
